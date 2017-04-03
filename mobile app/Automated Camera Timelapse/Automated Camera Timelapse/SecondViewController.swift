40d93c31841d6a11823742680d1c92e65f76b237
bkcan <briancanlas12345@gmail.com>
2017-04-03 12:50:36 -0500

    Add folders and mobile app

---
+++ mobile app/Automated Camera Timelapse/Automated Camera Timelapse/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Automated Camera Timelapse
+//
+//  Created by Brian Canlas on 4/3/17.
+//  Copyright © 2017 #squadgoals. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
